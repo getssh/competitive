a088c0f8bdd5b7a2b4c4ff5405c72f93aa00ff3b
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-02-25 16:28:42 +0300

    Time: 439 ms (14.36%), Space: 19.7 MB (47.17%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,9 +3,8 @@ class Solution:
         size = len(heights)
 
         for i in range(size):
-            j = i + 1
-            for j in range(size):
-                if heights[i] > heights[j]:
+            for j in range(i+1, size):
+                if (heights[i] < heights[j]):
                     heights[i], heights[j] = heights[j], heights[i]
                     names[i], names[j] = names[j], names[i]
         
