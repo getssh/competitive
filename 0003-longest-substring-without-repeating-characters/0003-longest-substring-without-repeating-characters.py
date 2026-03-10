4850d6f544582e6c0fad848c8d1d58b7e49f53ac
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-03-10 22:31:36 +0300

    Time: 15 ms (43.34%), Space: 19.2 MB (90.41%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,15 @@ class Solution:
         longest = ""
         left = 0
         
-        for right in range(1, len(s)):
+        for right in range(len(s)):
             if s[right] in s[left:right]:
                 if len(s[left:right]) > len(longest):
                     longest = s[left:right]
 
                 while s[right] in s[left:right] and left < right:
                     left += 1
-        
+
+        if len(s[left:]) > len(longest):
+            longest = s[left:]
+
         return len(longest)
