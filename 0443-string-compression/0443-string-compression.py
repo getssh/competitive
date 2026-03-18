8981e5f6ec2a4da1bfab323d8dd5a3067d791dbc
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-03-19 00:15:23 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,26 +1,47 @@
-from typing import List
-
 class Solution:
     def compress(self, chars: List[str]) -> int:
         group_num = 1
         left = 0
-        n = len(chars)
 
-        if n <= 1:
-            return n
+        if len(chars) == 1:
+            return 1
 
-        for right in range(1, n + 1):
-            if right < n and chars[right] == chars[right - 1]:
+        for right in range(1, len(chars)+1):
+            if right < len(chars) and chars[right] == chars[right - 1]:
                 group_num += 1
             else:
-                chars[left] = chars[right - 1]
-                left += 1
-                
-                if group_num > 1:
-                    for digit in str(group_num):
-                        chars[left] = digit
-                        left += 1
-                
-                group_num = 1
+                if group_num == 1:
+                    chars[left] = chars[right - 1]
+                    left += 1
+                    group_num = 1
+                elif group_num > 1 and group_num < 10:
+                    chars[left] = chars[right-1]
+                    chars[left+1] = str(group_num)
+                    left += 2
+                    group_num = 1
+                elif group_num >= 10 and group_num < 100:
+                    s = str(group_num)
+                    chars[left] = chars[right-1]
+                    chars[left+1] = s[0]
+                    chars[left+2] = s[1]
+                    left += 3
+                    group_num = 1
+                elif group_num >= 100 and group_num < 1000:
+                    s = str(group_num)
+                    chars[left] = chars[right-1]
+                    chars[left+1] = s[0]
+                    chars[left+2] = s[1]
+                    chars[left+3] = s[2]
+                    left += 4
+                    group_num = 1
+                elif group_num > 1000:
+                    s = str(group_num)
+                    chars[left] = chars[right-1]
+                    chars[left+1] = s[0]
+                    chars[left+2] = s[1]
+                    chars[left+3] = s[2]
+                    chars[left+4] = s[3]
+                    left += 5
+                    group_num = 1
 
         return left
