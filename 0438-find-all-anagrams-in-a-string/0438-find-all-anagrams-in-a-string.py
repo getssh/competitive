85f3642d894576517f0acf0e4a0322694fc81807
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-03-21 19:07:33 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,14 +1,44 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p = sorted(p)
-        res = []
+        checker = {}
+        window = {}
         left = 0
-        right = len(p) - 1
+        m = len(p)
+        n = len(s)
+        res = []
 
-        for right in range(right, len(s)):
-            if sorted(s[left:right+1]) == p:
-                res.append(left)
+        for i in range(m):
+            checker[p[i]] = checker.get(p[i], 0) + 1
+            window[s[i]] = window.get(s[i], 0) + 1
+
+        if checker == window:
+            res.append(0)
+
+        for right in range(m, n):
+            window[s[right]] = window.get(s[right], 0) + 1
+
+            window[s[left]] -= 1
+
+            if window[s[left]] == 0:
+                del window[s[left]]
+            
+            if window == checker:
+                res.append(right - m + 1)
+
             left += 1
         
-
         return res
+
+
+        # p = sorted(p)
+        # res = []
+        # left = 0
+        # right = len(p) - 1
+
+        # for right in range(right, len(s)):
+        #     if sorted(s[left:right+1]) == p:
+        #         res.append(left)
+        #     left += 1
+        
+
+        # return res
