d900c589ada9cfbc5684628d98d607a8be78f82b
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-04-22 23:08:45 +0300

    Time: 0 ms (100%), Space: 19.2 MB (89.43%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0: 
+            return 0
+        if x == 1:
+            return 1
+        
+        low = 1
+        high = x
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if mid * mid == x:
+                return mid
+
+            if mid * mid > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return low - 1
